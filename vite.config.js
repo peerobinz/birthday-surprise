7743bc36a80172fa756db03433a998c3c70f1452
Peerapat Visitsri <>
2025-05-30 23:18:59 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/birthday-surprise/', // ชื่อ repo ของคุณ ต้องมี / ข้างหน้าและข้างหลัง
   plugins: [react()],
 })
